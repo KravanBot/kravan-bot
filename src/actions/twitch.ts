e7114420499ab0bedf649d77819254c5b5542421
havividan4 <havividan4@gmail.com>
2026-05-05 21:07:51 +0300

    logging token

--- src/actions/twitch.ts
+++ src/actions/twitch.ts
@@ -83,6 +83,8 @@ export class Twitch {
   async getLive() {
     await this.#refreshToken();
 
+    console.log(this.#token);
+
     const res = await fetch(
       `https://api.twitch.tv/helix/streams?user_id=${Twitch.#BROADCASTER_ID}`,
       {
