2d5a744b81932dc4ef177cc3edaa3f668b8acb68
havividan4 <havividan4@gmail.com>
2026-05-12 16:45:28 +0300

    removing user mention on counting details

--- src/actions/counting.ts
+++ src/actions/counting.ts
@@ -15,7 +15,7 @@ import {
 } from "../utils/helpers.js";
 import { addCoins, hasEnoughCoins, takeCoins } from "../db/prisma.js";
 import { Mutex } from "async-mutex";
-import { client, tryToGetJackpot } from "../index.js";
+import { client, ranni_guild, tryToGetJackpot } from "../index.js";
 
 enum COUNT_EVENT {
   NORMAL = "Normal",
@@ -296,9 +296,10 @@ export class Counting {
         : "";
 
       await interaction.reply(
-        `Last counted number was ${this.#last_number} by ${userMention(
-          this.#last_counter_id,
-        )}${trapped_msg}.\n\n${current_event_msg}`,
+        `Last counted number was ${this.#last_number} by ${
+          ranni_guild?.members.cache.get(this.#last_counter_id)?.displayName ??
+          "someone sneaky that i cant find"
+        }${trapped_msg}.\n\n${current_event_msg}`,
       );
     });
   }
