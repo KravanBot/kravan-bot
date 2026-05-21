9dd76197fe794bde019522c51400652d027f7846
havividan4 <havividan4@gmail.com>
2026-05-22 00:21:45 +0300

    sending custom welcome messages

--- src/index.ts
+++ src/index.ts
@@ -436,6 +436,7 @@ const commands = [
     .setDescription("Get the weekly schedule"),
 ].map((cmd) => cmd.toJSON());
 
+let welcome_imgs_order: number[] | null = null;
 const guilds = [TEST_GUILD_ID, RANNI_GUILD_ID];
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
@@ -464,6 +465,49 @@ export const tryToGetJackpot = async (user: User, channel: Channel) => {
 
 const isGuildValid = (guild: Guild) => {
   return guilds.includes(guild.id);
+};
+
+const getWelcomeImgsOrder = async () => {
+  const NUM_OF_IMAGES = 19;
+
+  if (!!welcome_imgs_order) {
+    welcome_imgs_order = Array.from({ length: NUM_OF_IMAGES })
+      .fill(0)
+      .map((_, idx) => idx + 1);
+
+    function shuffle(array: any[]) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+
+    welcome_imgs_order = shuffle(welcome_imgs_order);
+
+    await fs.writeFile(
+      "src/info.json",
+      JSON.stringify(
+        {
+          welcome_imgs_order,
+        },
+        null,
+        2,
+      ),
+    );
+  } else {
+    const info = JSON.parse(await fs.readFile("src/info.json", "utf-8"));
+    welcome_imgs_order = info.welcome_imgs_order;
+
+    if (
+      !welcome_imgs_order?.length ||
+      welcome_imgs_order.length != NUM_OF_IMAGES
+    ) {
+      welcome_imgs_order = [];
+      return await getWelcomeImgsOrder();
+    }
+  }
 };
 
 (async () => {
@@ -522,6 +566,8 @@ client.once("clientReady", async () => {
   let last_announcement = (
     await twitch_channel.messages.fetch({ limit: 5 })
   ).find(({ author }) => author.id == client.user!.id);
+
+  await getWelcomeImgsOrder();
 
   setInterval(async () => {
     const handleNewMinute = async () => {
@@ -2241,6 +2287,65 @@ client.on("messageDelete", async (message) => {
 
       break;
   }
+});
+
+client.on("guildMemberAdd", async (member) => {
+  const welcome_channel = ranni_guild?.channels.cache.get(
+    "1387493763332571288",
+  );
+
+  if (!welcome_channel?.isSendable()) return;
+
+  let last_used_img = parseInt(
+    (await welcome_channel.messages.fetch({ limit: 1 }))
+      ?.at(0)
+      ?.embeds.at(0)
+      ?.image?.url.split(".")
+      .at(-2)
+      ?.split("/")
+      .at(-1) ?? "0",
+  );
+
+  console.log(
+    (await welcome_channel.messages.fetch({ limit: 1 }))
+      ?.at(0)
+      ?.embeds.at(0)
+      ?.image?.url.split("."),
+  );
+
+  if (isNaN(last_used_img)) last_used_img = 0;
+
+  if (!welcome_imgs_order) return;
+
+  const new_img_idx =
+    ((welcome_imgs_order?.indexOf(last_used_img) ?? -1) + 1) %
+    welcome_imgs_order.length;
+
+  if (new_img_idx == 0) await getWelcomeImgsOrder();
+
+  const img_filename = `${welcome_imgs_order[new_img_idx]}.jpg`;
+
+  const img = new AttachmentBuilder(
+    await fs.readFile(`./assets/welcome/${img_filename}`),
+    {
+      name: img_filename,
+    },
+  );
+
+  await welcome_channel.send({
+    content: userMention(member.id),
+    embeds: [
+      new CustomEmbed()
+        .setTitle(`Welcome Comrade ${member.displayName}`)
+        .setDescription(
+          `\u200b\n<#1336326581915881593>\n- concerning topics\n<#1311105291890196620>\n- for stream schedule\n<#1311121252827664516>\n- for propaganda\n\n<a:evilcat:1432751141254467815> Enjoy your stay mf <a:evilcat:1432751141254467815>`,
+        )
+        .setThumbnail(member.user.avatarURL())
+        .setImage(`attachment://${img_filename}`)
+        .setColor(0xff1c37),
+    ],
+    files: [img],
+  });
 });
 
 client.login(TOKEN);
