4e942045fa715bc6693f3e06126f55df2a811b54
havividan4 <havividan4@gmail.com>
2026-06-30 10:49:09 +0300

    fixing kravan cross not waiting for user reply

--- src/actions/kravan-cross.ts
+++ src/actions/kravan-cross.ts
@@ -11,6 +11,7 @@ import { CustomEmbed } from "../utils/embed.js";
 enum GameResult {
   WIN,
   LOSE,
+  CONTINUE,
 }
 
 export class KravanCross {
@@ -44,10 +45,7 @@ export class KravanCross {
           Math.floor(this.#bet * this.#multiplier),
         );
 
-        await interaction.editReply({
-          embeds: [new CustomEmbed().setDescription("WIN")],
-          components: [],
-        });
+        await this.#cashOut();
       } else {
         await interaction.editReply({
           embeds: [new CustomEmbed().setDescription("LOSE")],
@@ -77,77 +75,78 @@ export class KravanCross {
 
   async #handleGame() {
     for (let i = 0; i < KravanCross.#NUM_OF_STEPS; i++) {
-      const components = [
-        new ButtonBuilder()
-          .setCustomId("step")
-          .setLabel("➡️ Step")
-          .setStyle(ButtonStyle.Secondary),
-      ];
-
-      if (i > 0)
-        components.push(
+      const res = await new Promise<GameResult>(async (res) => {
+        const components = [
           new ButtonBuilder()
-            .setCustomId("cash-out")
-            .setLabel("💸 Cash Out")
-            .setStyle(ButtonStyle.Success),
-        );
+            .setCustomId("step")
+            .setLabel("➡️ Step")
+            .setStyle(ButtonStyle.Secondary),
+        ];
 
-      const msg = await this.#interaction.editReply({
-        embeds: [
-          new CustomEmbed().setDescription(
-            `\`Multiplier: x${this.#multiplier}\`\n\`Current Reward:\` <a:goldencoin:1311863385922736148> ${Math.floor(this.#bet * this.#multiplier).toLocaleString()}\n\nChoose <t:R>, or default behavior will be cache out`,
-          ),
-        ],
-        components: [
-          new ActionRowBuilder<ButtonBuilder>().addComponents(components),
-        ],
+        if (i > 0)
+          components.push(
+            new ButtonBuilder()
+              .setCustomId("cash-out")
+              .setLabel("💸 Cash Out")
+              .setStyle(ButtonStyle.Success),
+          );
+
+        const msg = await this.#interaction.editReply({
+          embeds: [
+            new CustomEmbed().setDescription(
+              `\`Multiplier: x${this.#multiplier}\`\n\`Current Reward:\` <a:goldencoin:1311863385922736148> ${Math.floor(this.#bet * this.#multiplier).toLocaleString()}\n\nChoose <t:R>, or default behavior will be cash out`,
+            ),
+          ],
+          components: [
+            new ActionRowBuilder<ButtonBuilder>().addComponents(components),
+          ],
+        });
+
+        const collector = msg.createMessageComponentCollector({
+          time: 60_000,
+          filter: (interaction) =>
+            interaction.user.id == this.#interaction.user.id,
+        });
+
+        collector.on("collect", async (interaction) => {
+          switch (interaction.customId) {
+            case "cash-out":
+              await interaction.deferReply();
+
+              return res(GameResult.WIN);
+
+            case "step":
+              await interaction.deferReply();
+
+              if (Math.floor(Math.random() * 2) == 0)
+                return res(GameResult.LOSE);
+
+              this.#multiplier += 0.5;
+
+              return res(GameResult.CONTINUE);
+          }
+
+          collector.stop("chose");
+        });
+
+        collector.on("end", async (_, reason) => {
+          if (reason != "time") return;
+
+          return res(GameResult.WIN);
+        });
       });
 
-      const collector = msg.createMessageComponentCollector({
-        time: 60_000,
-        filter: (interaction) =>
-          interaction.user.id == this.#interaction.user.id,
-      });
-
-      collector.on("collect", async (interaction) => {
-        switch (interaction.customId) {
-          case "cache-out":
-            await interaction.deferReply();
-
-            await this.#cacheOut();
-
-            return GameResult.WIN;
-
-          case "step":
-            await interaction.deferReply();
-
-            if (Math.floor(Math.random() * 2) == 0) {
-              return GameResult.LOSE;
-            }
-
-            this.#multiplier += 0.5;
-
-            break;
-        }
-
-        collector.stop("chose");
-      });
-
-      collector.on("end", async (_, reason) => {
-        if (reason != "time") return;
-
-        await this.#cacheOut();
-      });
+      if (res != GameResult.CONTINUE) return res;
     }
 
     return GameResult.WIN;
   }
 
-  async #cacheOut() {
+  async #cashOut() {
     await this.#interaction.editReply({
       embeds: [
         new CustomEmbed().setDescription(
-          `Nice! You cached out with <a:goldencoin:1311863385922736148> ${Math.floor(this.#bet * this.#multiplier)} coins!`,
+          `Nice! You cashed out with <a:goldencoin:1311863385922736148> ${Math.floor(this.#bet * this.#multiplier)} coins!`,
         ),
       ],
       components: [],
